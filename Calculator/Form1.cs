672ec7eea47ba524cc88b0f6129345f2e2c6fe98
Genesys <Genesys@DESKTOP-OUPN5F6>
2023-04-02 13:17:51 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (richTextBox1.Text != String.Empty)
+                this.richTextBox2.Text = this.richTextBox1.Text;
+            else
+                MessageBox.Show("TextBox1 is empty!");
+        }
+    }
+}
