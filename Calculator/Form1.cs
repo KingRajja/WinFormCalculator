5f70d95305bb51f06c781f0f6d8b2542a968685f
Genesys <chemodan2911@gmail.com>
2023-04-02 13:57:24 +0300

    ОПЗ вынесена в статический класс

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -20,47 +20,12 @@ namespace Calculator
 
         private void button1_Click(object sender, EventArgs e) {
             if (richTextBox1.Text != String.Empty)
-                this.richTextBox2.Text = MakeSolution(this.richTextBox1.Text);
+                this.richTextBox2.Text = PolishSolution.make(this.richTextBox1.Text);
             else
                 MessageBox.Show("TextBox1 is empty!");
         }
 
-        public string MakeSolution(string line) {
-            
-            Stack<double> st = new Stack<double>();
-
-            foreach(char arg in line) {
-                double num;
-                bool isNum = double.TryParse(arg.ToString(), out num);
-                if (isNum)
-                    st.Push(num);
-                else {
-                    double op2;
-                    switch (arg) {
-                    case '+':
-                        st.Push(st.Pop() + st.Pop());
-                        break;
-                    case '*':
-                        st.Push(st.Pop() * st.Pop());
-                        break;
-                    case '-':
-                        op2 = st.Pop();
-                        st.Push(st.Pop() - op2);
-                        break;
-                    case '/':
-                        op2 = st.Pop();
-                        if (op2 != 0.0)
-                            st.Push(st.Pop() / op2);
-                        else
-                            return "Ошибка. Деление на ноль";
-                        break;
-                    default:
-                        break;
-                    }
-                }
-            }
-            return st.Pop().ToString();
-        }
+        
 
         public void InterfaceRule() {
             this.richTextBox2.ReadOnly = true;
---
+++ Calculator/PolishSolution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator {
+    public static class PolishSolution {
+        private static Stack<double> st = new Stack<double>();
+
+        public static string make(string line) {
+            string returnValue = "";
+            foreach (char arg in line) {
+                double num;
+                bool isNum = double.TryParse(arg.ToString(), out num);
+                if (isNum)
+                    st.Push(num);
+                else {
+                    double op2;
+                    switch (arg) {
+                    case '+':
+                        st.Push(st.Pop() + st.Pop());
+                        break;
+                    case '*':
+                        st.Push(st.Pop() * st.Pop());
+                        break;
+                    case '-':
+                        op2 = st.Pop();
+                        st.Push(st.Pop() - op2);
+                        break;
+                    case '/':
+                        op2 = st.Pop();
+                        if (op2 != 0.0)
+                            st.Push(st.Pop() / op2);
+                        else {
+                            st.Clear();
+                            return "Ошибка. Деление на ноль";
+                        }
+                        break;
+                    default:
+                        break;
+                    }
+                }
+            }
+            returnValue = st.Pop().ToString();
+            st.Clear();
+            return returnValue;
+        }
+    }
+
+    
+}
