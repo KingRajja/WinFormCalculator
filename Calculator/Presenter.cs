1d9e68d3dd53fc2a12a169c2c7651397d1e93724
Genesys <chemodan2911@gmail.com>
2023-04-04 21:57:51 +0300

    Создали модель ОПЗ

--- Calculator/Presenter.cs
+++ Calculator/Presenter.cs
@@ -5,6 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Calculator {
+    
     internal class Presenter {
+        private List<string> log;
+        private IViewer viewer;
+
+        public Presenter(IViewer viewer) {
+            this.viewer = viewer;
+        }
+
+        public void popSymbolFromInfixFromula() {
+            string infixFormulaText = viewer.getTextFromTextBox(TextBoxName.infix);
+            if (infixFormulaText.Length > 0)
+                viewer.setTextInToTextBox(TextBoxName.infix, infixFormulaText.Remove(infixFormulaText.Length - 1));
+        }
+
+        private void showLog() {
+            viewer.setTextInToTextBox(TextBoxName.calcLog, String.Empty);
+            foreach (string line in log) {
+                viewer.setTextInToTextBox(TextBoxName.calcLog, $"{line}\n", true);
+            }
+            log.Clear();
+        }
+
+        public void addSymbolToInfixFormula(char symbol) { viewer.setTextInToTextBox(TextBoxName.infix, symbol.ToString(), true); }
+        public void makeSolution() { }
+        public void clear() {
+            viewer.setTextInToTextBox(TextBoxName.infix, String.Empty);
+            viewer.setTextInToTextBox(TextBoxName.postfix, String.Empty);
+            viewer.setTextInToTextBox(TextBoxName.solution, String.Empty);
+            viewer.setTextInToTextBox(TextBoxName.calcLog, String.Empty);
+        }
     }
 }
